3efabe50c6e705781f683475d6f887ba0a35873e
Jakub Kaczor <j.kaczor06@gmail.com>
2025-12-30 16:52:37 +0100

    added delay before restarting level after winning/crashing

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,13 +4,20 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    [SerializeField] float delayBeforeReload = 1.5f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if(collision.gameObject.layer == layerIndex)
         {
-            Scene CurrentScene = SceneManager.GetActiveScene();
-            SceneManager.LoadScene(CurrentScene.buildIndex);
+            Invoke("RestartLevel", delayBeforeReload);
         }
+    }
+
+    void RestartLevel()
+    {
+        Scene CurrentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(CurrentScene.buildIndex);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,12 +4,19 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] float delayBeforeReload = 1.5f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
         if(collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", delayBeforeReload);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
