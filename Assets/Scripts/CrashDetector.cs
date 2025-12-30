3e3ebb5c99b0a0220e7389ed9fa5cf14718a42c0
Jakub Kaczor <j.kaczor06@gmail.com>
2025-12-30 17:06:17 +0100

    created particle system for crash/win

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,13 @@ public class CrashDetector : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
     [SerializeField] float delayBeforeReload = 1.5f;
+    [SerializeField] ParticleSystem crashParticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if(collision.gameObject.layer == layerIndex)
         {
+            crashParticles.Play();
             Invoke("RestartLevel", delayBeforeReload);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,11 +6,13 @@ public class FinishLine : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     [SerializeField] float delayBeforeReload = 1.5f;
+    [SerializeField] ParticleSystem finishParticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
         if(collision.gameObject.layer == layerIndex)
         {
+            finishParticles.Play();
             Invoke("ReloadScene", delayBeforeReload);
         }
     }
