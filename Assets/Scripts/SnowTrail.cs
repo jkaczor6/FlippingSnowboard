7505bad1049ddf5f7144bd55e52be138d0d747c8
Jakub Kaczor <j.kaczor06@gmail.com>
2025-12-30 17:27:02 +0100

    created snow particles

---
+++ Assets/Scripts/SnowTrail.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SnowTrail : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] ParticleSystem snowParticles;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Floor");
+        if(collision.gameObject.layer == layerIndex)
+        {
+            snowParticles.Play();
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Floor");
+        if(collision.gameObject.layer == layerIndex)
+        {
+            snowParticles.Stop();
+        }
+    }
+    }
+}
