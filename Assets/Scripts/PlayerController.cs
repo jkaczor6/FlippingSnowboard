40e7de279fe0e93a1431a72675030074c860fd07
Jakub Kaczor <j.kaczor06@gmail.com>
2025-12-30 17:18:29 +0100

    created boost mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,28 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 20f;
+    [SerializeField] float boostSpeed = 25f;
     InputAction moveAction;
     Rigidbody2D rb2d;
+    SurfaceEffector2D se2d;
+    Vector2 moveVector;
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         rb2d = GetComponent<Rigidbody2D>();
+        se2d = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
         if(moveVector.x < 0)
         {
@@ -23,6 +33,17 @@ public class PlayerController : MonoBehaviour
         } else if (moveVector.x > 0)
         {
             rb2d.AddTorque(-torqueAmount);
+        }
+    }
+
+    void BoostPlayer()
+    {
+        if(moveVector.y > 0)
+        {
+            se2d.speed = boostSpeed;
+        } else
+        {
+            se2d.speed = baseSpeed;
         }
     }
 }
